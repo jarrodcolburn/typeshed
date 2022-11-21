f2c2785f8b3041bca5edd7d7c511f40edfdf56e0
jarrodcolburn <jcourtlandcolburn@gmail.com>
2022-11-21 06:53:18 +0000

    used create_baseline_stubs.py

---
+++ stubs/netCDF4/netCDF4/__init__.pyi
@@ -0,0 +1,23 @@
+from ._netCDF4 import *
+
+# Names in __all__ with no definition:
+#   CompoundType
+#   Dataset
+#   Dimension
+#   EnumType
+#   Group
+#   MFDataset
+#   MFTime
+#   VLType
+#   Variable
+#   chartostring
+#   date2index
+#   date2num
+#   get_alignment
+#   get_chunk_cache
+#   getlibversion
+#   num2date
+#   set_alignment
+#   set_chunk_cache
+#   stringtoarr
+#   stringtochar
--- stubs/netcdf4/netcdf4/__init__.pyi
+++
@@ -1,7 +0,0 @@
-from .compoundtype import *
-from .dataset import *
-from .enumtypes import *
-from .group import *
-from .types import *
-from .variables import *
-from .vltypes import *
