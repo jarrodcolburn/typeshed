eaf1ac1a91adc4af7c23e5d66a06e129667ab3ed
jarrodcolburn <jcourtlandcolburn@gmail.com>
2022-11-21 20:55:17 +0000

    out with the old, ...

--- stubs/netcdf4/netcdf4/compoundtype.pyi
+++
@@ -1,8 +0,0 @@
-class CompoundType:
-    """
-    A CompoundType instance is used to describe a compound data type, and can be passed to the the Dataset.createVariable method of a Dataset or Group instance. Compound data types map to numpy structured arrays. See CompoundType.__init__ for more details.
-
-    The instance variables dtype and name should not be modified by the user.
-    """
-
-    def __init__(group, datatype, datatype_name) -> None: ...
